f06ee723fca97cfd44d1aa84ad00a157b7edcae8
Jose Beltre <552006jose@gmail.com>
2026-02-10 23:32:33 -0400

    Agregar archivos de proyecto.

---
+++ SGCM.Applicaction.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace SGCM.Applicaction.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ SGCM.Persistence.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace SGCM.Persistence.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
