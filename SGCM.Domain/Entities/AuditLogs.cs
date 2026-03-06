20807b870c6952d17652002f2d3a86096d9e85a9
Jose Beltre <552006jose@gmail.com>
2026-03-06 19:13:37 -0400

    Implementar capa de dominio para AuditLogs

---
+++ SGCM.Domain/Entities/AuditLogs.cs
@@ -0,0 +1,26 @@
+﻿using SGCM.Domain.Base;
+using SGCM.Domain.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGCM.Domain.Entities
+{
+    public class AuditLogs : BaseEntity
+    {
+        public override int Id { get; set; }
+        public int UserId { get; set; }
+        public required EntityType EntityType { get; set; }
+        public int EntityId { get; set; }
+        public required Action Action { get; set; }
+        public string? PreviousValues { get; set; }
+        public string? NewValues { get; set; }
+        public string? IpAddress { get; set; }
+        public string? UserAgent { get; set; }
+        public DateTime ActionDate { get; set; }
+    }
+}
