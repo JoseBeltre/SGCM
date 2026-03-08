fad0c95ebdee34d8587a17df10bf3e6962ebfe95
Jose Beltre <552006jose@gmail.com>
2026-03-08 17:29:22 -0400

    Agregar repositorio de notifications en la capa de dominio

---
+++ SGCM.Domain/Repository/INotificationsRepository.cs
@@ -0,0 +1,14 @@
+﻿using SGCM.Domain.Base;
+using SGCM.Domain.Entities;
+using SGCM.Domain.Entities.Enums;
+
+namespace SGCM.Domain.Repository
+{
+    public interface INotificationsRepository : IBaseRepository<Notifications>
+    {
+        Task<OperationResult<List<Notifications>>> GetByUserIdAsync(int userId);
+        Task<OperationResult<List<Notifications>>> GetByAppointmentIdAsync(int appointmentId);
+        Task<OperationResult<List<Notifications>>> GetByStatusAsync(NotificationStatus status);
+        Task<OperationResult<List<Notifications>>> GetByTypeAsync(NotificationType type);
+    }
+}
