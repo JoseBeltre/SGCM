daf54968b26e6a9b19805ba2d84fc97b26fc64c3
Jose Beltre <552006jose@gmail.com>
2026-03-08 17:31:21 -0400

    Agregar respositorio para el modulo de availabiilities en la capa de dominio

---
+++ SGCM.Domain/Repository/IAvailabilityExceptionRepository.cs
@@ -0,0 +1,11 @@
+﻿using SGCM.Domain.Base;
+using SGCM.Domain.Entities;
+
+namespace SGCM.Domain.Repository
+{
+    public interface IAvailabilityExceptionRepository : IBaseRepository<AvailabilityExceptions>
+    {
+        Task<OperationResult<List<AvailabilityExceptions>>> GetByDoctorIdAsync(int doctorId);
+        Task<OperationResult<List<AvailabilityExceptions>>> GetByDateRangeAsync(int doctorId, DateTime startDate, DateTime endDate);
+    }
+}
