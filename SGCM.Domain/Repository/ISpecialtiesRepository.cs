29efb45b0e10dc2ee945192d33913d453f4d5ff7
Jose Beltre <552006jose@gmail.com>
2026-03-08 17:27:10 -0400

    Agregar respositorio en capa de dominio para Specialties

---
+++ SGCM.Domain/Repository/ISpecialtiesRepository.cs
@@ -0,0 +1,10 @@
+﻿using SGCM.Domain.Base;
+using SGCM.Domain.Entities;
+
+namespace SGCM.Domain.Repository
+{
+    public interface ISpecialtiesRepository : IBaseRepository<Specialties>
+    {
+        Task<OperationResult<List<Specialties>>> GetActiveAsync(); 
+    }
+}
