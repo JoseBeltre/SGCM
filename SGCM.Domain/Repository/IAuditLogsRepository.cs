4be3f5813c05a56b70e11f3ff2226582d37483e4
Jose Beltre <552006jose@gmail.com>
2026-03-08 17:32:04 -0400

    Agregar repositorio para AuditLog en la capa de dominio

---
+++ SGCM.Domain/Repository/IAuditLogsRepository.cs
@@ -0,0 +1,15 @@
+﻿using SGCM.Domain.Base;
+using SGCM.Domain.Entities;
+using SGCM.Domain.Entities.Enums;
+
+namespace SGCM.Domain.Repository
+{
+    public interface IAuditLogsRepository
+    {
+        Task<OperationResult> AddAsync(AuditLogs auditLog);
+        Task<OperationResult<AuditLogs>> GetByIdAsync(int logId);
+        Task<OperationResult<List<AuditLogs>>> GetByUserIdAsync(int userId);
+        Task<OperationResult<List<AuditLogs>>> GetByEntityAsync(EntityType entityType, int entityId);
+        Task<OperationResult<List<AuditLogs>>> GetByDateRangeAsync(DateTime startDate, DateTime endDate);
+    }
+}
