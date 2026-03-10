e9197a498c9421be51046af3abb47ce98f239127
Jose Beltre <552006jose@gmail.com>
2026-03-09 23:51:07 -0400

    Mover carpeta de Enums a Dominio

--- SGCM.Domain/Enums/ExceptionType.cs
+++ SGCM.Domain/Enums/ExceptionType.cs
@@ -4,12 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SGCM.Domain.Entities.Enums
+namespace SGCM.Domain.Enums
 {
-    public enum NotificacionStatus
+    public enum ExceptionType
     {
-        Pending,
-        Sent,
-        Failed,
+        Vacations,
+        Licence,
+        ExtraAvailability,
     }
 }
--- SGCM.Domain/Enums/NotificationStatus.cs
+++ SGCM.Domain/Enums/NotificationStatus.cs
@@ -4,12 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SGCM.Domain.Entities.Enums
+namespace SGCM.Domain.Enums
 {
-    public enum ExceptionType
+    public enum NotificacionStatus
     {
-        Vacations,
-        Licence,
-        ExtraAvailability,
+        Pending,
+        Sent,
+        Failed,
     }
 }
