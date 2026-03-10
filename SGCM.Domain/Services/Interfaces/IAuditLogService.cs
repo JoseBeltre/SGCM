fae3370d12b1fcb5730ab949803edc9037fb12a7
Jose Beltre <552006jose@gmail.com>
2026-03-10 13:42:44 -0400

    Implementar Service para auditoria

--- SGCM.Domain/Services/Interfaces/IAuditLogService.cs
+++ SGCM.Domain/Services/Interfaces/IAuditLogService.cs
@@ -1,23 +1,44 @@
 ﻿using SGCM.Domain.Base;
+using SGCM.Domain.Entities;
 using SGCM.Domain.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SGCM.Domain.Services.Interfaces
 {
     public interface IAuditLogService
     {
-        Task<OperationResult<bool>> RegisterLogAsync(
+        Task<OperationResult<AuditLog>> RecordCreateAsync<T>(
             int userId,
             EntityType entityType,
             int entityId,
-            Actions action,
-            string? previousValues,
-            string? newValues,
-            string? ipAddress,
-            string? userAgent);
+            T newEntity,
+            string? ipAddress = null,
+            string? userAgent = null);
+
+        Task<OperationResult<AuditLog>> RecordUpdateAsync<T>(
+            int userId,
+            EntityType entityType,
+            int entityId,
+            T previousEntity,
+            T newEntity,
+            string? ipAddress = null,
+            string? userAgent = null);
+
+        Task<OperationResult<AuditLog>> RecordDeleteAsync<T>(
+            int userId,
+            EntityType entityType,
+            int entityId,
+            T previousEntity,
+            string? ipAddress = null,
+            string? userAgent = null);
+
+        Task<OperationResult<AuditLog>> RecordCustomAsync(
+            int userId,
+            EntityType entityType,
+            int entityId,
+            AuditAction action,
+            object? previousValues = null,
+            object? newValues = null,
+            string? ipAddress = null,
+            string? userAgent = null);
     }
 }
