de4689c9471586050b423599266df02812e2300d
Jose Beltre <552006jose@gmail.com>
2026-03-11 00:04:59 -0400

    Agregar AppDbContext y Configuraciones para las entidades

---
+++ SGCM.Persistence/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGCM.Domain.Entities;
+
+namespace SGCM.Persistence.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+
+        public DbSet<Availability> Availabilities { get; set; }
+        public DbSet<AvailabilityException> AvailabilityExceptions { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Specialty> Specialty { get; set; }
+        public DbSet<AuditLog> AuditLogs { get; set; } 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}
