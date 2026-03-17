1cba6ad60f8baea18c6f125164d76ee3a6722b84
Jose Beltre <552006jose@gmail.com>
2026-03-16 22:17:03 -0400

    Refactoriza DependencyInjection y añade AddSGCM
    Se ha hecho pública y estática la clase DependencyInjection, añadiendo el método AddSGCM para registrar dependencias de notificaciones. También se han limpiado los using y añadido las referencias necesarias.

--- SGCM.IOC/Dependencies/DependencyInjection.cs
+++ SGCM.IOC/Dependencies/DependencyInjection.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
+using SGCM.IOC.Dependencies.Notification;
 
 namespace SGCM.IOC.Dependencies
 {
-    internal class DependencyInjection
+    public static class DependencyInjection
     {
+        public static IServiceCollection AddSGCM(IServiceCollection services)
+        {
+            services = NotificationDependency.Register(services);
+
+            return services;
+        }
     }
 }
