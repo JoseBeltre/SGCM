bb9be6c1b227eb8d7196deac6942570bcfb06a53
Aslan Guzmán <calogeromorel@gmail.com>
2026-03-17 01:18:26 -0400

    Add repository, services and interfaces

---
+++ SGCM.Domain/Repository/IAppointmentHistoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using SGCM.Domain.Base;
+using SGCM.Domain.Entities;
+
+namespace SGCM.Domain.Repository
+{
+    public interface IAppointmentHistoryRepository : IBaseRepository<AppointmentHistory>
+    {
+        Task<OperationResult<List<AppointmentHistory>>> GetByAppointmentIdAsync(int appointmentId);
+    }
+}
---
+++ SGCM.Domain/Services/Interfaces/IAppointmentService.cs
@@ -0,0 +1,13 @@
+﻿using SGCM.Domain.Base;
+
+namespace SGCM.Domain.Services.Interfaces
+{
+    public interface IAppointmentService
+    {
+        Task<OperationResult<bool>> CanBeCancelledAsync(int appointmentId);
+        Task<OperationResult<bool>> CanBeConfirmedAsync(int appointmentId);
+        Task<OperationResult<bool>> CanBeCompletedAsync(int appointmentId);
+        Task<OperationResult<bool>> CanBeRescheduledAsync(int appointmentId, DateTime newDate);
+        Task<OperationResult<bool>> HasSchedulingConflictAsync(int doctorId, DateTime date, int durationMinutes, int? excludeAppointmentId = null);
+    }
+}
