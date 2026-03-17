8eb0f32e8c8e326fb58309b88cc7f74069ded643
Jose Beltre <552006jose@gmail.com>
2026-03-17 01:45:21 -0400

    Fix: Faltaba agregar .Data para acceder a lso datos y validar existencia de doctores

--- SGCM.Domain/Services/SpecialtyDomainService.cs
+++ SGCM.Domain/Services/SpecialtyDomainService.cs
@@ -22,7 +22,7 @@ namespace SGCM.Domain.Services
                 return OperationResult<bool>.Failure("Specialty not found");
 
             var doctorsWithSpecialty = await _doctorRepository.GetDoctorsBySpecialtyIdAsync(specialtyId);
-            if (doctorsWithSpecialty.Any())
+            if (doctorsWithSpecialty.Data.Any())
                 return OperationResult<bool>.Failure("Specialty cannot be deactivated because there are doctors associated with it.");
 
             return OperationResult<bool>.Success(true, "Specialty can be safely deactivated.");
