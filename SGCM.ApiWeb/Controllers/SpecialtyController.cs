cc12c27f29569379467515a9ce6880a1fb254ce0
Jose Beltre <552006jose@gmail.com>
2026-03-17 13:41:20 -0400

    Primer Endpoint funcionando

---
+++ SGCM.ApiWeb/Controllers/SpecialtyController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SGCM.Application.Interfaces.Specialty;
+
+namespace SGCM.ApiWeb.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SpecialtyController : Controller
+    {
+        private readonly ISpecialtyService _specialtyService;
+        public SpecialtyController(ISpecialtyService specialtyService)
+        {
+            _specialtyService = specialtyService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _specialtyService.GetAllAsync();
+            if (!result.IsSuccess)
+                return NotFound();
+            return Ok(result);
+        }
+
+        //// GET: HomeController/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        //// GET: HomeController/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: HomeController/Create
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: HomeController/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: HomeController/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: HomeController/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: HomeController/Delete/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
