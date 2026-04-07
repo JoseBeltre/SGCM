45c721125e131fbb906b1e429e2c4947422f30ac
Jose Beltre <552006jose@gmail.com>
2026-04-07 15:06:21 -0400

    Init web

--- SGCM.Web/app/app.vue
+++ SGCM.Web/app/app.vue
@@ -1,32 +1,6 @@
-<script setup>
-useHead({
-  meta: [
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-  ],
-  link: [
-    { rel: 'icon', href: '/favicon.ico' }
-  ],
-  htmlAttrs: {
-    lang: 'en'
-  }
-})
-
-const title = 'Medica2 - SGCM'
-const description = 'Sistema de Gestión de Citas Médicas'
-
-useSeoMeta({
-  title,
-  description,
-  ogTitle: title,
-  ogDescription: description,
-  ogImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
-  twitterImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
-  twitterCard: 'summary_large_image'
-})
-</script>
-
 <template>
-  <UApp>
-    <NuxtPage />
-  </UApp>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
 </template>
